ee5f03dd4e5848fd88faf2f1fb272eb3c9146007
skorik-kirill <skorikkirill7@gmail.com>
2024-07-18 23:12:20 +0300

    update main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,10 @@
 resource "local_file" "inventory" {
   content = <<-EOT
-[app]
-localhost ansible_connection=local
+[server]
+54.197.218.16 ansible_user=ec2-user
+
+[server:vars]
+ansible_user=ec2-user
 EOT
   filename = "ansible/ansible_hosts"
 }
@@ -9,29 +12,36 @@ EOT
 resource "local_file" "playbook" {
   content = <<-EOT
 ---
-- name: Configure Application Server
-  hosts: app
-  connection: local
-  become: yes
-  
+- hosts: all
+  become: true
   tasks:
+    - name: Install nginx
+      dnf:
+        name: nginx
+        state: latest
+    - name: Restart Nginx
+      service:
+        name: nginx
+        state: restarted
     - name: Clone application code to host
       git:
         repo: https://github.com/skorik-kirill/test_task.git
         dest: /tmp/application
         update: yes
-    - name: Install Nginx
-      apt:
-        name: nginx
-        state: present
-    - name: Start Nginx Service
-      service:
-        name: nginx
-        state: started
+        force: yes
+    - name: install pip
+      dnf:
+        name: pip
+        state: latest
+      become: true
+    - name: copy nginx config 
+      copy:
+        src: /home/kyrylo/Python/test_task/ansible/nginx.conf
+        dest: /etc/nginx/nginx.conf
     EOT
     filename = "ansible/playbook.yml"
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i ansible/ansible_hosts ansible/playbook.yml"
+    command = "ansible-playbook  ansible/playbook.yml -u ec2-user"
   }
 }
