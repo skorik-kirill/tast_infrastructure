d3316079ffe54648ffb5c5d19fa6fcb5f2016739
skorik-kirill <skorikkirill7@gmail.com>
2024-07-19 14:10:08 +0300

    remove comments

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "test-kyrylo"
-    key            = "terraform/terraform.tfstate" ### This Must be UNIQUE PATH
+    key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
   }
 }
